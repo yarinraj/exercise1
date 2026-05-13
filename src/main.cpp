a36f688fae835778f1921ca7fd72b82f320eb756
MoranAshkenazi <moran.ash2000@gmail.com>
2026-05-13 10:28:57 +0300

    Task 45: implement command-line port argument handling with validation

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,37 @@
 #include "CommandParser.h"
 #include <string>
 #include "DataManager.h"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include<unistd.h>
 
-int main() {
-    DataManager dataManager("data/database.txt");
-    // DataManager dataManager("../data/database.txt");
-    std::string line;
-    
-    //a loop that runs forever , wating for an input line
-    while (true) {
-        std::getline(std::cin, line);
-        
-        //if the input is empty , the program continues
-        if (line.empty()) {
-            continue;
+
+
+int main(int argc, char*argv[]) {
+    //making sure the user wrote the port in the command line
+    if(argc<2){
+        std::cerr << "Usage: "<< argv[0]<<" <port>" << std::endl;
+        return 1; 
+    }
+    //my port
+    int port;
+    try{
+        port=std::stoi(argv[1]);
+//checking the pory is in the correct numbers
+        if(port<1024||port>65535){
+        std::cerr <<"Error: Port must be between 1024 and 65535." << std::endl;
+        return 1;
         }
         
-        parseCommand(line, dataManager);
+    }catch (const std::exception& e){
+        std::cerr << "Error: Invalid port number. Please provide a numeric value." << std::endl;
+    return 1;
     }
+    //socket
+    std::cout<<"server starting om port: "<<port << std::endl;
+    DataManager dataManager("data/database.txt");
+
+     return 0;
+ }
     
-    return 0;
-}
+   
