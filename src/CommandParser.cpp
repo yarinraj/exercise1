f76a83c5607e4f154fdafda74b4ed5c91edbce39
Noam <noam233a@gmail.com>
2026-05-18 08:48:48 +0300

    changed a liitle the client and the commandparser

--- src/CommandParser.cpp
+++ src/CommandParser.cpp
@@ -15,6 +15,11 @@ void parseCommand(const std::string& line, DataManager& dataManager) {
     
     //Retrieving the first word in the line (which is the name of the command)
     iss >> command;
+
+    //Changing all the letters in the command name to lower letters
+    for (char& c : command) {
+        c = std::tolower(c);
+    }
     
     if (command == "add") {
         executeAddCommand(line, iss, dataManager);
@@ -42,12 +47,13 @@ void parseCommand(const std::string& line, DataManager& dataManager) {
     else if (command == "help") {
         executeHelpCommand();
     } 
-    else if (command == "PATCH") {
+    else if (command == "patch") {
         std::string response = executePatchCommand(line, iss, dataManager);
         std::cout << response << std::endl;
     }
     else {
         //The user entered an unfamiliar command. So the program ignores the command and waits for the next .
-        return; 
+        std::cout << "400 Bad Request" << std::endl;
+        return;
     }
 }
