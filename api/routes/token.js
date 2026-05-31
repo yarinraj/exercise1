38d011cee306d4c3635fe05d9fc22fd8a7748b64
MoranAshkenazi <moran.ash2000@gmail.com>
2026-05-31 22:01:00 +0300

    new names

--- api/routes/token.js
+++ api/routes/token.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 //import the tokem controller we just created
-const tokenController = require('../controllers/tokenController');
+const tokenController = require('../controllers/token');
 // Route to handle POST requests for login (creates a token)
 // Since this router will be mapped to '/api/tokens' in app.js, 
 // the '/' here means the exact path '/api/tokens'
