f0dbd3e33060807a79f5a977cdc5072f051ae061
MoranAshkenazi <moran.ash2000@gmail.com>
2026-05-31 15:35:05 +0300

    feat: implement token verification route for login - EX1-91

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const port = 8080;
 
 // Import the user routes
 const userRoutes = require('./api/routes/userRoutes');
+//Import the token router
+const tokenRoutes = require('./api/routes/tokenRoutes');
 
 // Middleware - allows the server to parse incoming JSON in the request body
 app.use(express.json());
-
+//Map '/api/tokens' to our token router
+app.use('/api/tokens', tokenRoutes);
 // Mount the user routes to the base path '/api/users'
 // Any request starting with '/api/users' will be handled by userRoutes
 app.use('/api/users', userRoutes);
