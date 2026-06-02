e7b3c223155784f19a04fb353bc04258dd48a589
MoranAshkenazi <moran.ash2000@gmail.com>
2026-06-02 12:15:54 +0300

    fixing routes for get order with/without id

--- src/routes/order.js
+++ src/routes/order.js
@@ -8,7 +8,9 @@ const orderController = require('../controllers/order');
 router.post('/', orderController.createOrder);
 
 // Route to handle fetching all orders for the logged-in user (GET)
-router.get('/:id', orderController.getOrders);
+router.get('/', orderController.getOrders);
+// Route to fetch a specific order by its ID
+router.get('/:id', orderController.getOrderById);
 // Route to update a specific order by ID (PATCH)
 router.patch('/:id', orderController.updateOrder);
 // Route to delete a specific order by ID (delete)
