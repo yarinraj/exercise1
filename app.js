f2f5f1c87c7eb840d71e42c017d44c5fc649b714
Yarin Raj <yarin.raj1211@gmail.com>
2026-06-08 13:50:55 +0300

    EX1-123 creating user schema using mongoDB

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 const restaurantRoutes = require('./src/routes/restaurant');
 const userRoutes = require('./src/routes/user');
@@ -10,7 +11,7 @@ const orderRoutes = require('./src/routes/order');
 const restaurantController = require('./src/controllers/restaurant');
 
 // Middleware - allows the server to parse incoming JSON in the request body
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 
 app.use('/api/restaurants', restaurantRoutes);
 //Map '/api/tokens' to our token router
@@ -23,8 +24,20 @@ app.use('/api/orders', orderRoutes);
 //search rout
 app.get('/api/search/:query', restaurantController.searchItems);
 
+
+
 // Start the server and listen on the specified port
 const PORT = 8080;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/woltDB';
+console.log('Attempting to connect to MongoDB...'); // visual feedback for connection attempt (delete if not needed)
+// Connect to MongoDB using Mongoose
+mongoose.connect(MONGODB_URI, {serverSelectionTimeoutMS: 5000})
+  .then(() => {
+    console.log('Successfully connected to MongoDB!');
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+  });
