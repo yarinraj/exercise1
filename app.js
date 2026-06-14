660b0c6c76314f017ba73ff575dae51bc7a96afe
MoranAshkenazi <moran.ash2000@gmail.com>
2026-06-14 11:59:30 +0300

    fixing rout

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 const allowedOrigins = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'http://localhost:8080/'
+    'http://localhost:8080'
 ];
 
 const corsOptions = {
