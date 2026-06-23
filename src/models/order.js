03631cf77ba00f6a9831b73114648916dbb8a1de
Yarin Raj <yarin.raj1211@gmail.com>
2026-06-23 15:37:43 +0300

    minor fix

--- src/models/order.js
+++ src/models/order.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
     userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
+        type: string,
         required: true
     },
 
